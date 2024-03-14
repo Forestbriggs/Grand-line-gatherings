c211bfe3482c67390aa3fdea0bd5ba63b63769f7
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-03-13 20:08:16 -0700

    Add User session backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -7,6 +7,25 @@ const { User } = require('../../db/models');
 
 const router = express.Router();
 
+// Restore session user
+router.get('/', (req, res) => {
+    const { user } = req;
+
+    if (user) {
+        const safeUser = {
+            id: user.id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            username: user.username,
+            email: user.email
+        };
+
+        return res.json({
+            user: safeUser
+        });
+    } else return res.json({ user: null });
+});
+
 // Login
 router.post('/', async (req, res, next) => {
     const { credential, password } = req.body;
