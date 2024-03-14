c9abad7355af0cf42df93e32cfb3a3b84d753efc
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-03-13 19:23:18 -0700

    added first and last name to safeUser object in login method, also fixed type in "Listening on port" message

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -30,6 +30,8 @@ router.post('/', async (req, res, next) => {
 
     const safeUser = {
         id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
         email: user.email,
         username: user.username,
     };
