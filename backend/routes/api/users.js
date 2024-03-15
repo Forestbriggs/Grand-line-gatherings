24c9bbe410674a06af0a4a957739adf6a7effa7e
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-03-14 20:50:30 -0700

    Revert "removed a commented out fetch call from users.js"
    This reverts commit 4147bf4efceb3f216fce3627db854a0378473a3d.

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -59,3 +59,18 @@ router.post('/', validateSignup, async (req, res) => {
 });
 
 module.exports = router;
+
+// fetch('/api/users', {
+//     method: 'POST',
+//     headers: {
+//         "Content-Type": "application/json",
+//         "XSRF-TOKEN": `<value of XSRF-TOKEN cookie>`
+//     },
+//     body: JSON.stringify({
+//         firstName: 'Fire',
+//         lastName: 'Star',
+//         email: 'firestar@spider.man',
+//         username: 'Firestar',
+//         password: ''
+//     })
+// }).then(res => res.json()).then(data => console.log(data));
