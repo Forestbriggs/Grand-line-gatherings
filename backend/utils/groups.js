e6f258b8a16c36e6a05f416e0893cec743c9ab57
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-03-16 15:09:04 -0700

    added numMembers functionality to getAllGroups route

--- backend/utils/groups.js
+++ backend/utils/groups.js
@@ -2,16 +2,15 @@
 const { Group, User, sequelize } = require('../db/models');
 
 const getAllGroups = async (req, res) => {
-    const groups = await Group.findAll();
+    let groups = await Group.findAll();
 
-    //TODO create GroupMembers table and seed with data
     //TODO create Images table to hold group images
-    groups.map((group) => {
-        group.dataValues.numMembers = 1;
+    groups = await Promise.all(groups.map(async (group) => {
+        group.dataValues.numMembers = await group.countUsers();
         group.dataValues.previewImage = '/some/url'
 
         return group;
-    });
+    }));
 
     res.json({ Groups: groups })
 };
