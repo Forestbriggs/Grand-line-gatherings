e7b2c3a581f198b84ec6eeba6bdac7d96d6ad2c0
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-03-15 19:42:24 -0700

    refactored create-group-members migration file to use options object for production

--- backend/db/migrations/20240316012425-create-group-member.js
+++ backend/db/migrations/20240316012425-create-group-member.js
@@ -1,5 +1,11 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+    options.schema = process.env.SCHEMA;
+}
+
 module.exports = {
     async up(queryInterface, Sequelize) {
         await queryInterface.createTable('GroupMembers', {
@@ -32,9 +38,10 @@ module.exports = {
                 type: Sequelize.DATE,
                 defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
             }
-        });
+        }, options);
     },
     async down(queryInterface, Sequelize) {
-        await queryInterface.dropTable('GroupMembers');
+        options.table = 'GroupMembers'
+        await queryInterface.dropTable(options);
     }
 };
